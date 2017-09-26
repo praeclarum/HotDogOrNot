81a8bc5704645d45e6e41bc2cf6a59e5e5becc09
Frank A. Krueger <fak@praeclarum.org>
2017-09-26 14:43:52 -0700

    Use ARKit to show camera

--- HotDogOrNot/ViewController.cs
+++ HotDogOrNot/ViewController.cs
@@ -1,26 +1,47 @@
 ﻿using System;
 
 using UIKit;
+using ARKit;
 
 namespace HotDogOrNot
 {
 	public partial class ViewController : UIViewController
 	{
+		ARSCNView cameraView = new ARSCNView ();
+
 		protected ViewController (IntPtr handle) : base (handle)
 		{
-			// Note: this .ctor should not contain any initialization logic.
 		}
 
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
-			// Perform any additional setup after loading the view, typically from a nib.
+
+			cameraView.Frame = View.Bounds;
+			cameraView.AutoresizingMask = UIViewAutoresizing.FlexibleDimensions;
+			View.AddSubview (cameraView);
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+
+			var config = new ARWorldTrackingConfiguration {
+				WorldAlignment = ARWorldAlignment.Gravity,
+			};
+			cameraView.Session.Run (config, (ARSessionRunOptions)0);
+		}
+
+		public override void ViewWillDisappear (bool animated)
+		{
+			base.ViewWillDisappear (animated);
+
+			cameraView.Session.Pause ();
 		}
 
 		public override void DidReceiveMemoryWarning ()
 		{
 			base.DidReceiveMemoryWarning ();
-			// Release any cached data, images, etc that aren't in use.
 		}
 	}
 }
